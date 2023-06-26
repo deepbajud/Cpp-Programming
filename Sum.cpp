549fb4eb1c1e269ab48c748c8fd9d99b041e75c6
Deep Patel <118447327+deepbajud@users.noreply.github.com>
2023-06-26 23:24:07 +0530

    Sum
    sum of two integer numbers

---
+++ Sum.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int first_number, second_number, sum;
+    
+  cout << "Enter first  Number: ";
+  cin >> first_number: ;
+
+  cout << "Enter second  Number: ";
+  cin >> second_number: ;
+
+  // formula of sum of two number
+  sum = first_number + second_number;
+
+  // prints sum  
+  cout << first_number << " + " <<  second_number << " = " << sum;     
+
+  return 0;
+}
+
+
+
+//  output 
+//  first value = 2 
+//  second value = 2
+//  2 + 2 + 4
