fbd3a45ea61e43b9e71ed4b391588e5985f04577
Deep Patel <118447327+deepbajud@users.noreply.github.com>
2023-06-26 21:18:30 +0530

    vowel or constant
    check entered character vowel or constant

---
+++ vowel or constant.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+	char name;
+	
+	cout<<"Enter character :";
+	cin>>name;
+	
+	
+	if(name=='a' || name=='e'|| name=='i'|| name=='o'|| name=='u'){
+		cout<<"Is is vowel :";
+	}else{
+		cout<<"is constant:";
+		
+	}
+	return 0;	
+}
