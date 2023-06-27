445e41bc68d825417c2a793ba9e64422ed079dbf
Deep Patel <118447327+deepbajud@users.noreply.github.com>
2023-06-28 03:41:40 +0530

    Pattern 1
    pattern enter number by user and print like Pattern 1

---
+++ Pattern 1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int rows;
+
+    cout << "Enter any  Number: ";
+    cin >> rows;
+
+    for(int i = 1; i <= rows; ++i) {
+        for(int j = 1; j <= i; ++j) {
+            cout << j << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+	}
+
